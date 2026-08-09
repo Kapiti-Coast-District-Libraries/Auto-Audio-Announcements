a73115a33ee265fc98723f79ee97a693dde9d987
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-08-10 10:47:02 +1200

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -1,21 +1,21 @@
 // schedules.js
 const schedules = [
   {
-    time: "08:00",
+    time: "16:50",
     days: ["Mon", "Tue", "Wed", "Thu", "Fri"],
-    audio: { name: "Bell 1", url: "sounds/bell1.mp3" },
+    audio: { name: "Closing in 10", url: "sounds/bell1.mp3" },
     played: false
   },
   {
-    time: "12:00",
+    time: "16:55",
     days: ["Mon", "Wed", "Fri"],
-    audio: { name: "Bell 5", url: "sounds/bell2.mp3" },
+    audio: { name: "Closing in 5", url: "sounds/bell2.mp3" },
     played: false
   },
   {
-    time: "17:00",
-    days: ["Tue", "Thu"],
-    audio: { name: "Bell 12", url: "sounds/bell2.mp3" },
+    time: "21:00",
+    days: ["Mon", "Thu"],
+    audio: { name: "Extra access closing", url: "sounds/bell2.mp3" },
     played: false
   }
 ];
