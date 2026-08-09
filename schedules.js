04513c635fcd727745728e7e32289b274169ad10
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-08-10 09:53:58 +1200

    Create schedules.js

---
+++ schedules.js
@@ -0,0 +1,21 @@
+// schedules.js
+const schedules = [
+  {
+    time: "08:00",
+    days: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    audio: { name: "Bell 1", url: "sounds/bell1.mp3" },
+    played: false
+  },
+  {
+    time: "12:00",
+    days: ["Mon", "Wed", "Fri"],
+    audio: { name: "Bell 5", url: "sounds/bell5.mp3" },
+    played: false
+  },
+  {
+    time: "17:00",
+    days: ["Tue", "Thu"],
+    audio: { name: "Bell 12", url: "sounds/bell12.mp3" },
+    played: false
+  }
+];
